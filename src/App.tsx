dc4ad6b8030219f92ae73037a5b0b610471a684a
dwhddnjs <per1215@naver.com>
2024-01-31 23:59:44 +0900

    ✨ 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import { useRef, useState } from "react";
-import { SearchInput } from "./components/SearchInput";
-import { List } from "./components/List";
-import { useDebounce } from "./hooks/useDebounce";
-import { Modal } from "./components/Modal";
+import { useState } from "react"
 
-function App() {
-  const [searchValue, setSearchValue] = useState(0);
-
-  const debouncedSearchValue = useDebounce(searchValue, 1000);
-  const modalRef = useRef() as any;
-  const handleOpenModal = () => {
-    modalRef.current.openModal();
-  };
-
-  return (
-    <>
-      <SearchInput searchValue={searchValue} setSearchValue={setSearchValue} />
-      <List searchTerm={debouncedSearchValue} />
-      <div className="App1">
-        <Modal ref={modalRef} />
-        <button onClick={handleOpenModal}>Open</button>
-      </div>
-    </>
-  );
+type TodoTypes = {
+  id: number
+  title: string
+  completed: boolean
 }
 
-export default App;
+function App() {
+  const [todo, setTodo] = useState<TodoTypes[]>([
+    { id: 1, title: "아침 먹기", completed: true },
+    { id: 2, title: "점심 먹기", completed: false },
+    { id: 3, title: "저녁 먹기", completed: false },
+  ])
+  return (
+    <div>
+      <div>{todo[0].title}</div>
+      {todo.map((item) => (
+        <div key={item.id}>{item.title}</div>
+      ))}
+    </div>
+  )
+}
+
+export default App
